b419038bb40570103adeb6c8b4b83729c0351bca
Gabriel D <notprivategabe@gmail.com>
2016-08-16 11:57:28 -0400

    rewrote toodledo scripts into an object

---
+++ toodle_cli.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-'
+
+'''
+Way to interact with toodledo in a class
+'''
+
+import sys
+import json
+import time
+import pickle
+import urllib2
+import argparse
+import datetime
+
+from getpass import getpass
+from urllib import quote_plus
+from pprint import pprint
+
+
+import requests
+import requests.auth
+
+from requests_oauthlib import OAuth2Session
+
+
+class ToodleDoAuthCLI(object):
+    '''
+    Class to interact with Toodledo authorization/token creation.
+    '''
+    def __init__(self):
+        #self.token = token
+        self.account_url = 'https://api.toodledo.com/3/account/get.php?access_token={}'
+        self.tasks_url = 'https://api.toodledo.com/3/tasks/get.php?access_token={}'
+        self.authorization_base_url = 'https://api.toodledo.com/3/account/authorize.php'
+        self.token_url = 'https://api.toodledo.com/3/account/token.php'
+        self.code_url = '/3/account/authorize.php?response_type=code&client_id={}&scope={}&state={}'
+
+    def _load_app_details(self, app_details_pickle):
+        '''
+        Takes pickled hash with application details and return the attributes 
+        '''
+        app = pickle.load(open(app_details_pickle, 'rb'))
+        self.client_id = app['client_id']
+        self.client_secret = app['client_secret']
+        self.redirect_uri = app['redirect_uri']
+#        return self.client_id, self.client_secret, self.redirect_uri
+
+    def _auth_client(self):
+        '''
+        Take a pickled dict with app details and create an auth. 
+        Pickled hash must be done manually.
+        '''
+        # app_details = self._load_app_details(app_details_pickle)
+        # client_id = app_details[0]
+        # client_secret = app_details[1]
+        client_auth = requests.auth.HTTPBasicAuth(self.client_id, 
+            self.client_secret)
+        return client_auth
+
+    def _gen_scope(self, *args):
+        '''
+        Set the scope for requests. Often 'basic tasks write' is all that's
+        needed.
+        Old scope variable: scope = ['basic','tasks', 'write']
+        '''
+        out = []
+        for i in args:
+            out.append(i)
+        return out
+
+    def _get_auth_code(self):
+        '''
+        1. Generate auth url
+        2. Get request to auth url
+        3. Read Response
+        4. Post to response
+        5. Print redirect headers
+        '''
+        scope = self._gen_scope('basic tasks write')
+        toodle = OAuth2Session(self.client_id, 
+            scope=scope)
+        print toodle.scope
+        authorization_url, state = toodle.authorization_url(self.authorization_base_url)
+        print 'Visiting\n',authorization_url
+        s = requests.Session()
+        response = s.get(authorization_url)
+        print response.status_code
+        data = {
+            'scope':scope,
+            'response_type':'code',
+            'client_id':self.client_id,
+            'state':state,
+            'playground':'0',
+            'email':raw_input('Email: ')
+            'pass':getpass('Pass: '),
+            'authorized':'Sign In'
+        }
+        # The connection will fail because it goes to local host. We want to
+        # Parse the URL and grab the code because it's been generated at this point
+        try:
+            r2 = s.post(authorization_url, data=data)
+            print r2.url, r2.status_code
+        except requests.ConnectionError as err:
+            code = str(err).split('?')
+            code = code[1].split('&')
+            code = code[0].split('=')[1]
+        return code, state
+
+    def get_token(self):
+        '''
+        Sends Toodledo the necessary POST to generate an auth token after getting 
+        the state and code from _get_auth_code().
+
+        get_token() stores the full token as a pickle file so it can be loaded later
+        or used to refresh the token.
+        '''
+        code, state = self._get_auth_code()
+        client_auth = self._auth_client()
+        post_data = {
+            'grant_type':'authorization_code',
+            'code':code,
+            'redirect_uri':self.redirect_uri,
+            'state':state,
+            'device':'terminal'
+        }
+        response = requests.post(self.token_url, auth=client_auth, data = post_data)
+        full_token = response.json()
+        pickle.dump(full_token, open('auth_token.pkl', 'wb'))
+        print 'Created token {}'.format(full_token['access_token'])
+
+    def refresh(self):
+        '''
+        Loads the pickled auth token and makes the necessary POST request to 
+        Toodledo in order to refresh the token. It then pickles the new token.
+        '''
+        client_auth = self._auth_client() 
+        full_token = pickle.load( open('auth_token.pkl', 'rb'))
+        post_data = {
+            'grant_type':'refresh_token',
+            'refresh_token':full_token['refresh_token'],
+            'device':'terminal'
+        }
+        response = requests.post(self.token_url, auth=client_auth, data = post_data)
+        full_token = response.json()
+        pickle.dump(full_token, open('auth_token.pkl', 'wb'))
+        print 'Refreshed Token {}'.format(full_token['refresh_token'])
+
+class ToodleDoCLI():
+    '''
+    Class to interact with Toodledo.
+    '''
+    def __init__(self, token):
+        self.token = token
+        self.token = self._load_token(token)
+        self.token = self.token['access_token']
+        self.account_url = 'https://api.toodledo.com/3/account/get.php?access_token='
+        self.tasks_get_url = 'https://api.toodledo.com/3/tasks/get.php?access_token='
+        self.json_tasks = self._parse_to_json(self.sync_tasks())
+        print self.json_tasks
+
+    def _load_token(self, token):
+        self.token = pickle.load( open(token, 'rb'))
+        return self.token
+
+    def sync_tasks(self):
+        '''
+        Performs a synchronization with ToodleDo and dumps out tasks to a pickle
+        '''
+        get_tasks = requests.get('{}{}'.format(self.account_url, self.token))
+        pickle.dump(get_tasks.text, open('tasks_queried.pkl', 'wb'))
+        return pickle.load(open('tasks_queried.pkl', rb))
+
+    def _parse_to_json(self, task_pickle):
+        '''
+        Takes output of sync_tasks and converts into a json 
+        '''
+        data = pickle.load( open(task_text, 'rb'))
+        return json.loads(data)
+
+    def _print_all_tasks(self):
+        '''
+        Prints out all tasks in a for loop 
+        '''
+        for i in range(len(self.json_tasks)):
+            print self.json_tasks[i]
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--new-token', action='store_true',
+        help='Authorize a new app and generate a new oauth_token')
+    parser.add_argument('-r', '--refresh-token', action='store_true',
+        help='Refresh the current auth_token.pkl file')
+    args = parser.parse_args()
+    if args.new_token:
+        toodle = ToodleDoAuthCLI()
+        toodle._load_app_details('app_details.pkl')
+        toodle.get_token()
+    if args.refresh_token:
+        toodle = ToodleDoAuthCLI()
+        toodle._load_app_details('app_details.pkl')
+        toodle.refresh()
+    toodle = ToodleDoCLI('auth_token.pkl')
+    a = toodle.sync_tasks()
+    toodle._print_all_tasks()
+if __name__ == '__main__':
+    main()
